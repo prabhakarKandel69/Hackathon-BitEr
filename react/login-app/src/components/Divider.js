30e806035acd26861f019f54f101284f57b17139
Bit-Er <078bct021.pushkar@sagarmatha.edu.np>
2024-11-28 11:58:11 +0545

    divider

--- react/login-app/src/components/Divider.js
+++ react/login-app/src/components/Divider.js
@@ -4,7 +4,7 @@ const Divider = () => {
   return (
     <div className="flex items-center my-6">
       <div className="flex-grow border-t border-gray-300"></div>
-      <span className="mx-2 text-gray-500">OR</span>
+      <span className="mx-2 text-gray-600">OR</span>
       <div className="flex-grow border-t border-gray-300"></div>
     </div>
   );
